9002468897b3be75db26aa850405840941828244
미니 <cow970814@naver.com>
2023-03-27 13:41:44 +0900

    refactor : Login View 키보드 없애는 로직 추가

--- ProjectManager/ProjectManager/AuthView/LoginView.swift
+++ ProjectManager/ProjectManager/AuthView/LoginView.swift
@@ -82,6 +82,7 @@ private extension LoginView {
     
     var loginButton: some View {
         Button {
+            UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
             ViewStore(store).send(.tapLoginButton)
         } label: {
             Text("로그인하기")
